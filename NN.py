7e18fb1d1fa3b6820dd6815dbb6f1c24ba3c01ff
LJXSean <risingsun221@gmail.com>
2024-04-05 01:30:40 +0800

    create NN.py

--- NN.py
+++ NN.py
@@ -1,212 +1,166 @@
-import numpy as np
 import pandas as pd
+import json
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
+data_path_aclarc = "./acl-arc/scaffolds/sections-scaffold-train.jsonl"
+data_path_scicite = "./scicite/scaffolds/sections-scaffold-train.jsonl"
+with open(data_path_aclarc, encoding='utf-8') as data_file:
+    data = [json.loads(line) for line in data_file]
+    df = pd.DataFrame(data).drop_duplicates()
 
-import matplotlib.pyplot as plt
+sort_cols_section_paper = ['section_name', 'cited_paper_id']
+sort_cols_section = ['section_name']
+sort_cols = sort_cols_section_paper
+final_cols = ['text', 'text_pos', 'section_name', 'citing_paper_id', 'cited_paper_id']
+
+def split_and_concatenate(group):
+    # Calculate the split index
+    split_index = len(group) // 2
+    
+    # Split the group into two halves
+    first_half = group.iloc[:split_index].reset_index(drop=True)['text']
+    second_half = group.iloc[split_index:].reset_index(drop=True)
+    second_half.rename(columns={'text': 'text_pos'}, inplace=True)
+
+    # Concatenate the halves horizontally
+    concatenated = pd.concat([first_half, second_half], axis=1)
+    return concatenated
+
+# Gets samples using concatenation
+def get_pos_samples_concat(df):
+    df_concat = df.copy(deep=True)
+
+    # Dummy columns for groupby, to keep original columns
+    include_groups = [i + '_drop' for i in sort_cols]
+    df_concat[include_groups] = df_concat[sort_cols]
+    
+    result = df_concat.groupby(include_groups).apply(split_and_concatenate, include_groups=False).reset_index(drop=True)
+    return result
+
+concat = get_pos_samples_concat(df)
+
+# Replace NA with text_pos (dropout in roberta will treat this as unsupervised learning)
+def handle_na(df):
+    df.loc[pd.isna(df['text']), 'text'] = df.loc[pd.isna(df['text'])]['text_pos']
+
+handle_na(concat)
+
+concat[['text', 'text_pos']].to_csv('data_file.csv', index=False)
+
+from transformers import AutoTokenizer
+from datasets import load_dataset
+
+dataset = load_dataset("csv", data_files="data_file.csv")
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+column_names = dataset['train'].column_names
+
+# Testing
+def tokenize(examples, max_length=256):
+    id_masks_all_cols = []
+    col_name = column_names[0]
+    length = len(examples[col_name])
+
+    # Tokenize examples for each column
+    for k in column_names:
+        id_mask = tokenizer(examples[k], truncation=True, padding='max_length', max_length=max_length)
+        id_masks_all_cols.append(id_mask)
+
+    zipped_id_mask = {}
+    id_mask_col = id_masks_all_cols[0]
+
+    # Zips all columns together for each feature, input_id/attention_mask
+    for feature in id_mask_col:
+        zipped_id_mask[feature] = [[id_mask[feature][i] for id_mask in id_masks_all_cols] for i in range(length)]
+
+    return zipped_id_mask
+
+# Shape = [#features, #sentences, #samples(anchor, pos, neg)]
+tokenized = dataset['train'].map(tokenize, batched=True, remove_columns=column_names)
+
+from torch.utils.data import DataLoader
+
+tokenized.set_format("torch")
+
+small_train_dataset = tokenized.shuffle(seed=42).select(range(1000))
+train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=32)
+
+for batch in train_dataloader:
+    # Shape = [#featuress, #batch_size, #tensor_length]
+    print(batch['input_ids'].shape)
+    break
 
 import torch
 import torch.nn as nn
 
-from torch.utils.data import TensorDataset, DataLoader
+def contrastive_loss(embeddings, temperature=0.1):
+    sents_per_vector = embeddings.size(1)
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-import re
-
-# load the data
-dataset = pd.read_csv('train.csv')
-kaggle = pd.read_csv('test.csv')
-
-def preprocess(sentence):
-    sentence = sentence.strip().lower()
-    sentence = re.sub(r'[^\w\s!?"\']', '', sentence)
-
-    tokens = word_tokenize(sentence)
-    lemmatizer = WordNetLemmatizer()
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    return ' '.join(tokens)
-
-X = dataset['Text']#.apply(lambda x: preprocess(x['Text']), axis=1, result_type='expand')
-K = kaggle['Text']#.apply(lambda x: preprocess(x['Text']), axis=1, result_type='expand')
-y = dataset['Verdict'] + 1
-
-# Split sentences and labels into training and test set with a test set size of 20%
-sentences_train, sentences_test, labels_train, labels_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-print("Size of training set: {}".format(len(sentences_train)))
-print("Size of test set: {}".format(len(sentences_test)))
-
-# Create Term-Document Matrix for different n-gram sizes
-#vectorizer = TfidfVectorizer(ngram_range=(1, 2), max_features=8000)
-vectorizer = TfidfVectorizer(max_features=2472,norm='l1',ngram_range=(1,1),stop_words='english',strip_accents='ascii',analyzer='word')
-#vectorizer = CountVectorizer(ngram_range=(1, 1), max_features=10000)
-
-y_train = np.asarray(labels_train, dtype=np.int8)
-y_test = np.asarray(labels_test, dtype=np.int8)
-
-# Vectorize both training and test set
-vectorizer.fit(sentences_train)
-X_train = vectorizer.transform(sentences_train)
-X_test = vectorizer.transform(sentences_test)
-K_test = vectorizer.transform(K)
-
-# The default Tensor stores float values
-X_train = torch.Tensor(X_train.todense())
-X_test = torch.Tensor(X_test.todense())
-K_test = torch.Tensor(K_test.todense())
-
-# Our labels are integers, hence we use LongTensor
-# (that's required, otherwise we would get an error later)
-y_train = torch.LongTensor(y_train)
-y_test = torch.LongTensor(y_test)
-
-print(X_train.shape, y_train.shape)
-print(X_test.shape, y_test.shape)
-print(K_test.shape)
-
-dataset_train = TensorDataset(X_train, y_train)
-dataset_test = TensorDataset(X_test, y_test)
-dataset_k = TensorDataset(K_test, K_test[:, 0])
-
-batch_size = 64
-
-loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
-loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=True)
-loader_k = DataLoader(dataset_k, batch_size=batch_size, shuffle=True)
-
-class SimpleNet2(nn.Module):
+    if sents_per_vector < 2 or sents_per_vector > 3:
+        raise Exception("Unexpected number of sentences per sample received. Expected: 2/3") 
     
-    def __init__(self, vocab_size):
-        super().__init__()
-        self.vocab_size = vocab_size
-        
-        self.net = nn.Sequential(
-            nn.Linear(self.vocab_size, 16),
-            nn.ReLU(),
-            nn.Linear(16, 8),
-            nn.ReLU(),
-            nn.Linear(8, 3),
-            nn.LogSoftmax(dim=1)
-        )
-        
-    def forward(self, X):
-        log_probs = self.net(X)
-        return log_probs
+    cos_sim = nn.CosineSimilarity(dim=-1)
+
+    # Reshape to 3D for broadcast computation
+    anchor = embeddings[:, 0].unsqueeze(1)
+    positive = embeddings[:, 1].unsqueeze(0)
     
-# Create the model and move to device
-classifier = SimpleNet2(X_train.shape[1])
+    # Pairwise cosine similarity, shape = [batch_size, batch_size]
+    pairwise_sim = cos_sim(anchor, positive)
 
-print(classifier)
+    # index of positive sample for corresponding anchors (matrix diagonal)
+    target = torch.arange(pairwise_sim.size(0))
 
-def evaluate(model, loader):
-
-    # Set model to "eval" mode (not needed here, but a good practice)
-    model.eval()
-
-    # Collect predictions and ground truth for all samples across all batches
-    y_pred, y_test = [], []
-
-        
-    # Loop over each batch in the data loader
-    for X_batch, y_batch in loader:
-        # Push batch through network to get log probabilities for each sample in batch
-        log_probs = model(X_batch)                
-
-        # The predicted labels are the index of the higest log probability (for each sample)
-        y_batch_pred = torch.argmax(log_probs, dim=1)
-
-        # Add predictions and ground truth for current batch
-        y_test += list(y_batch.detach().numpy())
-        y_pred += list(y_batch_pred.detach().numpy())
-
-    # Set model to "train" mode (not needed here, but a good practice)
-    model.train()            
-            
-    # Return the f1 score as the output result
-    return metrics.f1_score(y_test, y_pred, average='macro')
-
-def train(model, loader_train, loader_test, optimizer, criterion, num_epochs):
-
-    losses, f1_train, f1_test = [], [], []
+    # Horizontally concatenate hard_neg similarities (if any)
+    if sents_per_vector == 3:
+        hard_neg = embeddings[:, 2].unsqueeze(0)
+        hard_neg_sim = cos_sim(anchor, hard_neg)
+        pairwise_sim = torch.cat([pairwise_sim, hard_neg_sim], 1)
     
-    # Set model to "train" mode (not needed here, but a good practice)
-    model.train()
+    pairwise_sim /= temperature
 
-    # Run all epochs
-    for epoch in range(1, num_epochs+1):
+    loss = nn.CrossEntropyLoss()
+    output = loss(pairwise_sim, target)
 
-        # Initialize epoch loss (cummulative loss fo all batchs)
-        epoch_loss = 0.0
+    return output
 
-        # Loop over each batch in the data loader
-        for X_batch, y_batch in loader_train:
-            # Push batch through network to get log probabilities for each sample in batch
-            log_probs = classifier(X_batch)                
+from torch.optim import AdamW
+from transformers import RobertaModel
 
-            # Calculate loss
-            loss = criterion(log_probs, y_batch)
+def train(batch):
+    input_ids = batch['input_ids']
+    attention_mask = batch['attention_mask']
+    batch_size, sents_per_vector, tensor_size = input_ids.shape
 
-            ### PyTorch Magic! ###
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    # Flatten to encode all at once
+    input_ids = torch.reshape(input_ids, (-1, tensor_size))
+    attention_mask = torch.reshape(attention_mask, (-1, tensor_size))
 
-            # Keep track of overall epoch loss
-            epoch_loss += loss.item()
+    # Use [CLS] token representation
+    outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+    embeddings = outputs.last_hidden_state[:, 0]
 
+    # Reshape back to nested tensors
+    embeddings = torch.reshape(embeddings, (batch_size, sents_per_vector, -1))
+    return embeddings
+
+model = RobertaModel.from_pretrained('roberta-base')
+optimizer = AdamW(model.parameters(), lr=5e-5)
+epochs = 2
+
+for epoch in range(epochs):
+    total_loss = 0
+    # Shape = [#features, #batch_size, #tensor_length]
+    for i, batch in enumerate(train_dataloader):
+        optimizer.zero_grad()
+
+        embeddings = train(batch)
+        loss = contrastive_loss(embeddings)
+        loss.backward()
+        optimizer.step()
         
-        # Keep track of all epoch losses
-        losses.append(epoch_loss)
-        
-        # Compute f1 score for both TRAINING and TEST data
-        f1_tr = evaluate(model, loader_train)
-        f1_te = evaluate(model, loader_test)
-        f1_train.append(f1_tr)
-        f1_test.append(f1_te)
+        total_loss += loss.item()
 
-        print("Loss:\t{:.3f}, f1 train: {:.3f}, f1 test: {:.3f} (epoch {})".format(epoch_loss, f1_tr, f1_te, epoch))
-     
-    # Return all losses and f1 scores (all = for each epoch)
-    return losses, f1_train, f1_test        
-
-# Create the model and movie to device
-classifier = SimpleNet2(X_train.shape[1])
-
-# Define loss function
-criterion = nn.NLLLoss()
-
-# Define optimizer (you can try, but the basic (Stochastic) Gradient Descent is actually not great)
-optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001, weight_decay=1e-5)
-
-num_epochs = 25
-
-losses, f1_train, f1_test = train(classifier, loader_train, loader_test, optimizer, criterion, num_epochs)
-
-def predict(model, loader):
-
-    # Set model to "eval" mode (not needed here, but a good practice)
-    model.eval()
-
-    # Collect predictions and ground truth for all samples across all batches
-    y_pred = []
-
-    # Loop over each batch in the data loader
-    for X_batch, y_batch in loader:        
-        with torch.no_grad():
-            log_probs = model(X_batch)                
-            y_batch_pred = torch.argmax(log_probs, dim=1)
-            y_pred += list(y_batch_pred.detach().numpy())
-
-    model.train()            
-    return y_pred
-
-result = predict(classifier, loader_k)
-kaggle['Verdict'] = pd.Series(result) - 1
-kaggle.drop(columns=['Text'], inplace=True)
-kaggle.to_csv('A0233694X.csv', index=False)
-
-
-
+        if i % 4 == 0:
+            print(f"Batch: {i+1}/{len(train_dataloader)}, Loss: {total_loss/(i+1)}")
+    
+    print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_dataloader)}")
